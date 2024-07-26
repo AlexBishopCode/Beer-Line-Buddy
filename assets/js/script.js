9bd24359d31dc00dc7d7782799512596c45018f0
Alexander Bishop <2kok8sho@students.codeinstitute.net>
2024-07-26 04:25:32 +0000

    import fontawesome icons and add java to icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,42 @@ document.getElementById('next-button').addEventListener('click', () => {
 });
 
 updateStep(); // Initialize with the first step
+
+
+// ICONS
+
+document.addEventListener("DOMContentLoaded", function () {
+    let icons = document.querySelectorAll(".icon");
+    icons[0].classList.add("clickable"); // Make the first icon clickable
+
+    icons.forEach((icon, index) => {
+        icon.addEventListener("click", function () {
+            if (icon.classList.contains("clickable")) {
+                icon.classList.remove("clickable");
+                if (index + 1 < icons.length + 1) {
+                    icons[index + 1].classList.change("clickable");
+                    icons[index + 1].classList.add("clickable");
+                    icons[index].classList.remove("fa-circle");
+                    icons[index].classList.add("fa-circle-check");
+                }
+            }
+        });
+    });
+});
+
+// // Change Div Color 
+function ticketBox(event) {
+    const targetDivId = event.target.getAttribute('data-target');
+    const targetDiv = document.getElementById(targetDivId);
+    
+    if (targetDiv) {
+        targetDiv.style.backgroundColor = '#2d6ed9';
+        targetDiv.style.borderRadius = '0 0 10px 10px';
+        targetDiv.style.transition = '0.7s';
+    }
+}
+// Add click event
+
+document.querySelectorAll('.fa-circle').forEach(faCircleElement => {
+    faCircleElement.addEventListener('click', ticketBox);
+});
